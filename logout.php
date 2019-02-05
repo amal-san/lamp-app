a0a7396465936f263ab66d02ad217484e2229fb4
Amalsanthoshp <amaldemaniax@gmail.com>
2019-02-05 21:31:01 +0530

    logout

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+           session_start(); 
+
+	   // Destroy Session 
+	   $_SESSION = [];  
+	   session_unset();
+	   session_destroy(); 
+
+	   header("Location:http://localhost/temp.github.io/login.html");
+	   exit();
+
+?>
+
