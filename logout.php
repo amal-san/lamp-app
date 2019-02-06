1b91a3518f7738ee9e4b7675286e767b667a1bbf
Amalsanthoshp <amaldemaniax@gmail.com>
2019-02-06 18:06:27 +0530

    session_destroy

--- logout.php
+++ logout.php
@@ -2,17 +2,13 @@
            session_start(); 
 
 	   // Destroy Session 
-<<<<<<< HEAD
 	   $_SESSION = [];  
 	   session_unset();
-=======
-	   $_SESSION = array();  
-	   session_unset(); 
->>>>>>> 1ab84358d05c9c5e42e470aa7f4ad6cd83b7f1f1
 	   session_destroy(); 
 
 	   header("Location:http://localhost/temp.github.io/login.html");
 	   exit();
 
 ?>
+
 
